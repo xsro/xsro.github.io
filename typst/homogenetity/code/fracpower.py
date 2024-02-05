833867543f1ffb2bf9c34b65b8ba70108a153d00
Liu Y Cheng <xsro@foxmail.com>
2024-02-05 22:30:59 +0800

    1

--- typst/homogenetity/code/fracpower.py
+++ typst/homogenetity/code/fracpower.py
@@ -5,8 +5,8 @@ from scipy.integrate import solve_ivp
 configs=[
     (r"$\dot{x}=-x$",lambda t,x:-x**1,),
     (r"$\dot{x}=-\mathrm{sign}(x)$",lambda t,x:-np.sign(x),),
-    (r"$\dot{x}=-\sqrt[3]{x}$",lambda t,x:-x**(1/3),),
-    (r"$\dot{x}=-\mathrm{sign}(x)|x|^2$",lambda t,x:-np.sign(x)*x**2,),
+    (r"$\dot{x}=-\sqrt[3]{x}$",lambda t,x:-np.sign(x)*np.abs(x)**(1/3),),
+    (r"$\dot{x}=-\mathrm{sign}(x)|x|^2$",lambda t,x:-np.sign(x)*np.abs(x)**2,),
 ]
 
 
@@ -14,11 +14,16 @@ configs=[
 if __name__=="__main__":
     import matplotlib.pyplot as plt
     for i,(latex,rhs) in enumerate(configs):
-        plt.subplot(2,2,i+1)
-        tfinal=100 if i==3 else 10
+        plt.subplot(4,2,2*i+2)
+        tfinal=40 if i==3 else 10
         sol=solve_ivp(rhs,[0,tfinal],np.array([1.]),t_eval=np.linspace(0,tfinal,1000))
         plt.grid(True)
         plt.plot(sol.t,sol.y.T)
         plt.title(latex)
+        plt.subplot(4,2,2*i+1)
+        x=np.linspace(-1,1,1000)
+        y=np.array(list(map(lambda x:-rhs(0,x),x)))
+        plt.grid(True)
+        plt.plot(x,y)
     plt.tight_layout()
     plt.show()
