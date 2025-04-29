4c97bfa81ffb4497edf78023efbbaeb3733f42fe
Chasen Liu (Cheng Liu) <xsro@foxmail.com>
2025-04-30 02:55:20 +0800

    add typst

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,7 @@
 PROJECT_ROOT=`pwd -P`
 
 wget https://github.com/typst/typst/releases/download/v0.13.1/typst-x86_64-unknown-linux-musl.tar.xz -O /tmp/typst.tar.xz
+mkdir -p /tmp/typst
 tar -xf /tmp/typst.tar.xz -C /tmp/typst
-export PATH=$PATH:/tmp/typst/typst-x86_64-unknown-linux-musl/bin"
+export PATH=$PATH:/tmp/typst/typst-x86_64-unknown-linux-musl/"
 python typst/cp.py
