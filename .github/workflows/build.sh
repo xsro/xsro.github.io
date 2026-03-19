0487d86f4942d06c2e2812959ee0b1b6a2b02972
Chasen Liu <xsro@foxmail.com>
2026-03-19 20:43:14 +0800

    remove install step for Dart Sass

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,6 @@
 PROJECT_ROOT=`pwd -P`
 
-wget https://github.com/typst/typst/releases/download/v0.13.1/typst-x86_64-unknown-linux-musl.tar.xz -O /tmp/typst.tar.xz
+wget https://github.com/typst/typst/releases/download/v0.14.2/typst-x86_64-unknown-linux-musl.tar.xz -O /tmp/typst.tar.xz
 mkdir -p /tmp/typst
 tar -xf /tmp/typst.tar.xz -C /tmp/typst
 export PATH="$PATH:/tmp/typst/typst-x86_64-unknown-linux-musl/"
